3039b962520cf59e37fd7cc467e1acf2d8c11587
fdgardon <119641606+fdgardon@users.noreply.github.com>
2022-12-10 22:35:23 -0800

    feature/add-js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,36 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics. length; x++) {
+var randomtopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+ for (var x = 0; x < topics. length; x++) {
     console.log(topics[x]);
 }
 
-/*
-
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
 }
 
-*/
+
+
+function selecttopic() {
+
+  if (topics[0] === 'HTML') {
+  console.log("Let's study HTML!");
+ } else if (topics[0] === 'CSS') {
+  console.log("Let's study CSS!");
+ } else if (topics[0] === 'Git') {
+  console.log("Let's study Git!");
+ } else if (topics[0] === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+ } else {
+  console.log('Please try again!');
+ }
+
+}
+
+console.log('Here are the topics we learned through Prework:');
+
+listTopics()
+
+console.log('Which topic should we study first');
+
+selecttopic()
